e27e0fc8cb0bc8eff705480f846e4f46862e1536
Christophe Frézier <frezier@gmail.com>
2023-11-06 00:03:06 +0100

    Improve scrolling

--- cfrezier.js
+++ cfrezier.js
@@ -1,16 +1,31 @@
 const options = document.querySelectorAll('.conf-option');
 const slides = document.querySelectorAll('.conf-type');
 const detail = document.querySelector('.detail');
+let previouslyActive = 0;
+
+const scrollTo = (index) => {
+    // scroll
+    let toPass = 0;
+    for (let j = 0; j < index; j++) {
+        toPass += slides[j].getBoundingClientRect().height
+    }
+    detail.scrollTop = toPass;
+}
+
 for (let i = 0; i < options.length; i++) {
     options[i].addEventListener('click', () => {
-        let toPass = 0;
-        for(let j = 0; j < i; j++) {
-            toPass += slides[j].getBoundingClientRect().height
-        }
-        detail.scrollTop = toPass;
+        // set active
+        options[previouslyActive].classList.remove('active');
+        options[i].classList.add('active');
+        previouslyActive = i;
+
+        scrollTo(i);
     });
 }
 
+
 screen.orientation.addEventListener("change", (event) => {
-    detail.scrollTop = 0;
+    setTimeout(() => {
+        scrollTo(previouslyActive);
+    }, 10);
 });
