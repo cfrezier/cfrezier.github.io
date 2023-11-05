22c4eca97fbc86356d1ccdde416d1f8b2aff263c
Christophe Frézier <frezier@gmail.com>
2023-11-06 00:40:37 +0100

    Improve scrolling

--- cfrezier.js
+++ cfrezier.js
@@ -5,7 +5,7 @@ let previouslyActive = 0;
 
 const scrollTo = (index, smooth = true) => {
     // scroll
-    slides[index].scrollIntoView({behavior: smooth ? "smooth" : "instant", block: "start", inline: "start"});
+    slides[index].scrollIntoView({behavior: smooth ? "smooth" : "instant", block: "end", inline: "end"});
 }
 
 for (let i = 0; i < options.length; i++) {
